bc5c81a9d2709d5cf13c434cff06780848957e9c
FadwaElouah <fadwaelouah@gmail.com>
2025-09-02 10:55:57 +0100

    test push c

---
+++ .c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char nom[20];
+    char prenom[20], adresseEmaial[30], sexe[20];
+    int age;
+    printf("entrer votre nom :");
+    scanf("%s",&nom);
+    printf("entrer votre prenom :");
+    scanf("%s",&prenom);
+    printf("entrer votre age :");
+        scanf("%d",&age);
+        printf("entrer votre nom :");
+    scanf("%s",&sexe);
+    // Write C code here
+    printf("Try programiz.pro");
+
+    return 0;
+}
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
