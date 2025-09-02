2dc9dab5bd16aab73ac905ba09761ce1a6899c34
siham el aoumari <sihamelaoumari@gmail.com>
2025-09-02 14:00:40 +0100

    exercice 3

---
+++ chalengev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	float km ;
+	float m;
+	printf("la vitesse en km:");
+	scanf("%f",&km);
+	m = km*0.27778;
+	printf("la vitesse en minut par seconde est:%f",m);
+	
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+	return 0;
+}
