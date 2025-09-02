533f642f3226dce30eea96c8034eb8d0a2d72e54
siham el aoumari <sihamelaoumari@gmail.com>
2025-09-02 11:55:12 +0100

    exercice 3

--- chalenge.c
+++ chalenge.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+int main(){
+	int distanceKm;
+	int yard;
+	printf(" la distance en Km :");
+	scanf("%d",&distanceKm);
+	yard= distanceKm*1093.61;
+	printf("la distance en yard est:%d",yard);
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
 	return 0;
 }
